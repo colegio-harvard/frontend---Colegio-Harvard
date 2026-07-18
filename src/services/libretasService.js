bb791cbab2348fdbbb67c9a282943b49ffcf3bcf
Harvard <mbafjpg@gmail.com>
2026-07-18 09:48:30 -0500

    Agregar boton para personalizar frase institucional

--- src/services/libretasService.js
+++ src/services/libretasService.js
@@ -13,6 +13,7 @@ export const cargarNotas = params => apiClient.get('/libretas/notas', { params }
 export const guardarNotas = data => apiClient.post('/libretas/notas', data);
 export const guardarComentarioDocente = data => apiClient.post('/libretas/comentarios-docente', data);
 export const guardarAcompanamiento = data => apiClient.post('/libretas/acompanamiento', data);
+export const guardarFraseInstitucional = frase => apiClient.put('/libretas/frase-institucional', { frase });
 export const crearOpcionCatalogo = data => apiClient.post('/libretas/catalogo', data);
 export const cargarMerito = params => apiClient.get('/libretas/merito', { params });
 export const cargarLibreta = id => apiClient.get(`/libretas/libreta/${id}`);
