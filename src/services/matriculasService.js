e427cf9c81a479d0e2a037c3a033c3959a78b57d
colegio-harvard <mbafjpg@gmail.com>
2026-08-28 17:21:46 -0500

    Primera versión de Matrícula Digital

---
+++ src/services/matriculasService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import apiClient from './apiClient';
+import { API_URL } from '../utils/constants';
+
+const publicClient = axios.create({ baseURL: API_URL, headers: { 'Content-Type': 'application/json' } });
+
+export const cargarMatriculas = () => apiClient.get('/matriculas/bootstrap');
+export const guardarConfiguracionMatricula = (data) => apiClient.put('/matriculas/configuracion', data);
+export const generarInvitacionMatricula = (id_alumno) => apiClient.post('/matriculas/invitar', { id_alumno });
+export const obtenerExpedienteMatricula = (id) => apiClient.get(`/matriculas/${id}`);
+export const revisarMatricula = (id, data) => apiClient.put(`/matriculas/${id}/revisar`, data);
+export const obtenerMatriculaPublica = (token) => publicClient.get(`/matriculas/publica/${token}`);
+export const aceptarMatriculaPublica = (token, data) => publicClient.post(`/matriculas/publica/${token}/aceptar`, data);
+
